f262fc75ce5c42d9ee1220983759e19f663daeb7
Mason France <masonrf1@gmail.com>
2020-05-05 16:39:41 -0600

    Add Home Route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
-  resources :dishes
-  resources :vendors
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  namespace :api do
+    namespace :v1 do
+      resources :dishes
+      resources :vendors
+    end
+  end
+
+  root to: "home#index"
+
 end
