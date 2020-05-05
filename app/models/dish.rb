10f6fb582ba29fec386b378045b2213546ea86ea
Mason France <masonrf1@gmail.com>
2020-05-05 16:30:28 -0600

    Update models

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -1,2 +1,5 @@
 class Dish < ApplicationRecord
+  belongs_to :user
+
+  validates :name, presence: true
 end
--- app/models/vendor.rb
+++ app/models/vendor.rb
@@ -1,3 +1,5 @@
 class Vendor < ApplicationRecord
-  belongs_to :user
+  has_many :dishes, dependent: :destroy
+  
+  validates :name, presence: true
 end
